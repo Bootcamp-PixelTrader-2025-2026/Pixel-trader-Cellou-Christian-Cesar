364f781aeed28fda6e8859a1f97dd500508d4845
pemen26 <christian.kouatan_pemen@edu.devinci.fr>
2026-01-16 12:47:18 +0100

    Suppression de la limite SQL pour afficher tout le stock (55 jeux)

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, jsonify
-from flask_cors import CORS  # <-- Ajoute cette ligne
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
-CORS(app)  # <-- Ajoute cette ligne pour autoriser le site web
+CORS(app) # Pour que le navigateur accepte de parler à l'API
 
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root", # Change si ton utilisateur est différent
-        password="kpc2005", # Mets ton mot de passe Workbench
+        user="root",
+        password="kpc2005",
         database="pixel_trader"
     )
 
 @app.route('/jeux', methods=['GET'])
 def get_jeux():
     conn = get_db_connection()
+    # dictionary=True permet d'avoir les noms des colonnes dans le JSON
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM Jeux LIMIT 20")
+    
+    # J'ai retiré le "LIMIT 20" pour afficher tout le stock (55 jeux)
+    cursor.execute("SELECT * FROM Jeux")
+    
     resultats = cursor.fetchall()
     cursor.close()
     conn.close()
     return jsonify(resultats)
 
 if __name__ == '__main__':
+    # debug=True pour voir les erreurs en direct pendant l'oral
     app.run(debug=True)
